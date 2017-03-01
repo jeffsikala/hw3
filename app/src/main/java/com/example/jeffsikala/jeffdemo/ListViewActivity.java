b553ab28e7eb1be7de08ecbfa6cd36e41d0bc5fa
Jeff Sikala <jsikala1@student.gsu.edu>
2017-03-01 18:51:52 -0500

    hw3 upgrade

--- app/src/main/java/com/example/jeffsikala/jeffdemo/ListViewActivity.java
+++ app/src/main/java/com/example/jeffsikala/jeffdemo/ListViewActivity.java
@@ -1,36 +1,126 @@
 package com.example.jeffsikala.jeffdemo;
 
+import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ListAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import adapter.ListViewAdapter;
+import com.example.jeffsikala.jeffdemo.adapter.ListViewAdapter;
+
+import java.util.ArrayList;
 
 public class ListViewActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private ListView listView;
+    private ArrayList listResult;
+    private ArrayList<Fragment> fragmentList = new ArrayList<Fragment>();
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
-        intitalView();
+
+        listResult  = new ArrayList<String>();
+        createFakeResult();
+        initialView();
     }
 
-    private void intitalView(){
-        listView = (ListView) findViewById(R.id.list_view);
-        ListAdapter listViewAdapter = new ListViewAdapter(this);
+    private void createFakeResult(){
+        listResult.add("AAAAAAAAAAAA");
+        listResult.add("BBBB");
+        listResult.add("CCCCCCCC");
+        listResult.add("DDDD");
+        listResult.add("EEEEEEEEEEE");
+        listResult.add("FF");
+        listResult.add("GGGGGG");
+        listResult.add("HHHHH");
+        listResult.add("IIII");
+        listResult.add("JJJJJJJ");
+        listResult.add("KKKKKKKKKK");
+        listResult.add("LLLLL");
+        listResult.add("MMMM");
+        listResult.add("NNN");
+
+    }
+
+    private void initialView() {
+        listView = (ListView)findViewById(R.id.list_view);
+
+
+
+//        View view = getLayoutInflater().inflate(R.layout.listviewheader, null);
+//        LinearLayout listViewHeader = (LinearLayout)view.findViewById(R.id.list_view_header);
+
+
+        View pager =  getLayoutInflater().inflate(R.layout.activity_view_pager, null);
+        ViewPager viewPager = (ViewPager)pager.findViewById(R.id.view_pager);
+
+
+
+//        fragmentList.add(new LoginFragment());
+//        fragmentList.add(new ContentFragment());
+//        fragmentList.add(new HistorysFragment());
+        fragmentList.add(new CenterCropFragment());
+        fragmentList.add(new CenterFragment());
+        fragmentList.add(new CenterInsideFragment());
+        fragmentList.add(new FitCenterFragment());
+        fragmentList.add(new FitEndFragment());
+        fragmentList.add(new FitStartFragment());
+        fragmentList.add(new FitXYFragment());
+        fragmentList.add(new MatrixFragment());
+
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this.getSupportFragmentManager());
+        viewPagerAdapter.setContent(fragmentList);
+
+//        TabLayout tabLayout = (TabLayout) pager.findViewById(R.id.tab_layout);
+//        tabLayout.setupWithViewPager(viewPager);
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+        ListViewAdapter listViewAdapter = new ListViewAdapter(this, listResult);
+        listView.addHeaderView(pager);
+
+
+//
+//        ListViewAdapter listViewAdapter = new ListViewAdapter(this, listResult);
+//        listView.addHeaderView(listViewHeader);
+
+        TextView tv = new TextView(this);
+        tv.setText("We have no more content");
+        tv.setTextSize(28);
+        tv.setGravity(Gravity.CENTER);
+        listView.addFooterView(tv);
+
+
         listView.setAdapter(listViewAdapter);
         listView.setOnItemClickListener(this);
-
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(this, "listView was clicked at position: " + position,Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "listView was clicked at position:"+position, Toast.LENGTH_SHORT).show();
+        UtilLog.logD("ListViewActivity", String.valueOf(position));
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        Intent intent = new Intent();
+        intent.putExtra("message", "ListView");
+        setResult(RESULT_OK, intent);
+        super.onBackPressed();
+
     }
 }
--- app/src/main/java/com/example/jeffsikala/jeffdemo/MainActivity.java
+++ app/src/main/java/com/example/jeffsikala/jeffdemo/MainActivity.java
@@ -3,47 +3,102 @@ package com.example.jeffsikala.jeffdemo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-    private ImageButton bt1;
-    private ImageButton bt3;
+public class MainActivity extends BaseActivity {
+
+    private ImageButton btn1;
+    private ImageButton btn3;
+
+    @OnClick(R.id.btn2)
+    public void button2Click(){
+        Intent intent = new Intent(this, DialogActivity.class);
+        startActivityForResult(intent, 2);
+        //toActivity(DialogActivity.class);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        intialView();
-        intialListener();
+        initialView();
+        initialListener();
+        ButterKnife.bind(this);
     }
+
+
 
     private void initialView(){
 
-        bt1 = (ImageButton) findViewById(R.id.bt1);
-        bt3 = (ImageButton) findViewById(R.id.bt3);
+        btn1 =(ImageButton)findViewById(R.id.btn1);
+        btn3 =(ImageButton)findViewById(R.id.btn3);
     }
 
-    private void intialListener(){
-        bt1.setOnClickListener(new View.OnClickListener(){
+    // Uses Reflection to search source code for on click method
+
+    private void initialListener(){
+
+        // Android Best Practice for Listeners
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
-          public void onClick(View v){
-               Toast.makeText(v.getContext(),"Button1 was clicked", Toast.LENGTH_LONG).show();
+            public void onClick(View v) {
+
+                toastShort("Button 1 was tapped");
+                Intent intent = new Intent(v.getContext(), ViewPagerActivity.class);
+                intent.putExtra("key", "value");
+                Bundle bundle = new Bundle();
+                bundle.putInt("Integer", 12345);
+                Book book = new Book();
+                book.setName("Android");
+                book.setAuthor("Viraj");
+                bundle.putSerializable("book",book);
+
+                intent.putExtras(bundle);
+                startActivityForResult(intent,1);
+                //toActivity(ViewPagerActivity.class);
             }
         });
-        bt3.setOnClickListener(new View.OnClickListener(){
+
+        btn3.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                Intent intent = new Intent(v.getContext(), ListViewActivity.class);
-                startActivity(intent);
+            public void onClick(View v) {
+
+                toastShort("Button 2 was clicked!");
+                Intent intent = new Intent(v.getContext(),ListViewActivity.class);
+                startActivityForResult(intent,3);
             }
         });
     }
 
-    public void onClick(View v){
-        Toast.makeText(v.getContext(),"Button2 was clicked", Toast.LENGTH_LONG).show();
+    @Override
+    protected void onStart(){
+        super.onStart();
+        toastShort("onStart");
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case 1:
+                String message = data.getStringExtra("message");
+                toastShort(message);
+                break;
+            case 2:
+                toastShort("Dialog");
+                break;
+            case 3:
+                toastShort("ListView");
+                break;
+            default:
+        }
+
+    }
 }
