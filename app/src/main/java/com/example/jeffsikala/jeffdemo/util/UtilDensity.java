24d7d44f963437ac4bd017a5047c7154f15a17d6
Jeff Sikala <jsikala1@student.gsu.edu>
2017-03-01 19:34:30 -0500

    added the until

---
+++ app/src/main/java/com/example/jeffsikala/jeffdemo/util/UtilDensity.java
@@ -0,0 +1,21 @@
+package com.example.jeffsikala.jeffdemo.util;
+
+import android.content.Context;
+
+/**
+ * Created by Jeff on 3/1/17.
+ */
+
+public class UtilDensity {
+
+    public static int dip2px(Context context, float dpValue){
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    public static int px2dip(Context context, float pxValue){
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+}
